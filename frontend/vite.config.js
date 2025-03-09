addef704cb192815cc29a1fa8193268431e74916
satyajit-patel <satyajitpatel770001@gmail.com>
2025-03-09 15:18:59 +0530

    added backend link at proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'https://myportfolio-x1df.onrender.com'
     },
   },
   plugins: [react(), tailwindcss(),],
