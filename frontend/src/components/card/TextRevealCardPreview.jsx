0b5daf3b04a3bb9913d7a1cfbe4ccd88d2d791ef
satyajit-patel <satyajitpatel770001@gmail.com>
2025-04-14 01:29:06 +0530

    bug fix 4

--- frontend/src/components/card/TextRevealCardPreview.jsx
+++ frontend/src/components/card/TextRevealCardPreview.jsx
@@ -9,19 +9,19 @@ import {FlipWordsDemo} from "../wordFlip/FlipWordsDemo";
 export function TextRevealCardPreview() {
   return (
     (
-    <div className="h-screen">
-    <FlipWordsDemo />
-    <div
-      className="flex items-center justify-center bg-black h-[40rem] w-full">
-      <TextRevealCard text="You know the business" revealText="Satyajit Patel ">
-        <TextRevealCardTitle>
-          About Me
-        </TextRevealCardTitle>
-        <TextRevealCardDescription>
-          I'm Satyajit Patel, a proud loud introvert who loves coding, getting my hands dirty building tech, and exploring unmet technologies. I'm currently pursuing an MTech in AI & Data Science at VIT Bhopal, diving deep into C++, GenAI, algorithms, and all things tech. When I'm not experimenting with new ideas, you'll find me enjoying a good game of cricket.
-        </TextRevealCardDescription>
-      </TextRevealCard>
-    </div>
+    <div className="bg-black">
+      <FlipWordsDemo />
+      <div
+        className="flex items-center justify-center bg-black h-[40rem] w-full">
+        <TextRevealCard text="You know the business" revealText="Satyajit Patel ">
+          <TextRevealCardTitle>
+            About Me
+          </TextRevealCardTitle>
+          <TextRevealCardDescription>
+            I'm Satyajit Patel, a proud loud introvert who loves coding, getting my hands dirty building tech, and exploring unmet technologies. I'm currently pursuing an MTech in AI & Data Science at VIT Bhopal, diving deep into C++, GenAI, algorithms, and all things tech. When I'm not experimenting with new ideas, you'll find me enjoying a good game of cricket.
+          </TextRevealCardDescription>
+        </TextRevealCard>
+      </div>
     </div>)
   );
 }
